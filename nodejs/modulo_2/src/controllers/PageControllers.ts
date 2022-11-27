cd531dc04f2689bd11a0008859bf8b07cf64b176
Maicon Souza <maicon_souza09@hotmail.com>
2022-11-27 12:07:21 -0300

    creating the complete base of the petDevShop project

---
+++ nodejs/modulo_2/src/controllers/PageControllers.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express"
+
+export function home(req: Request, res: Response){
+
+}
+
+export function dogs(req: Request, res: Response){
+    
+}
+
+export function cats(req: Request, res: Response){
+    
+}
+
+export function fishes(req: Request, res: Response){
+    
+}
