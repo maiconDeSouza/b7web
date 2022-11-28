a99a8f760d08cf241d714ba81200cfa691393c41
Maicon Souza <maicon_souza09@hotmail.com>
2022-11-27 21:43:21 -0300

    up

--- nodejs/modulo_2/src/controllers/PageControllers.ts
+++ nodejs/modulo_2/src/controllers/PageControllers.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express"
 
 export function home(req: Request, res: Response){
-
+    res.status(200).render('pages/page', {
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    })
 }
 
 export function dogs(req: Request, res: Response){
